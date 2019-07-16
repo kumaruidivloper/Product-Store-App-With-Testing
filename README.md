bae9211530fa7c1160305b51a99226e1e39d2c21
kumarshan <kumaruidivloper@gmail.com>
2019-07-17 00:48:28 +0530

    App-Folder-Structure

--- README.md
+++ README.md
@@ -31,3 +31,7 @@ Step3: Create some initial Data in db.json file
 Step4: Edit package.json start property ["start": "concurrently \"ng serve\" \"json-server --watch db.json\" ",]
 Step5: npm install concurrently
 Step6: npm run start [For run the app]
+
+
+## Application_Folder_Structure_Image
+![IMG_4789](https://user-images.githubusercontent.com/30646609/60645546-9246cb80-9e56-11e9-9d96-68d57a7e20b7.JPG)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './features/home/home.component';
+import { ProductsComponent } from './features/products/products.component';
+import { ProductFormComponent } from './features/product-form/product-form.component';
+import { ProductDetailComponent } from './features/product-detail/product-detail.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    ProductFormComponent,
+    ProductDetailComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
